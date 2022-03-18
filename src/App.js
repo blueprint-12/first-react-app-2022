1efd9d73f877957b7f77936317095e7e2445da5a
blueprint-12 <blueprint12@daum.net>
2022-03-18 22:10:19 +0900

    create To Do List

--- src/App.js
+++ src/App.js
@@ -3,36 +3,37 @@ import styles from './App.module.css';
 import { useEffect, useState } from 'react';
 
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState('');
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
+  const [toDo, setToDo] = useState('');
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === '') {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    //input 내용 비워주기
+    setToDo('');
+  };
+  console.log(toDos);
 
-  useEffect(() => {
-    console.log('I run only once');
-  }, []);
-
-  useEffect(() => {
-    console.log("I run when 'keyword' changes");
-  }, [keyword]);
-
-  useEffect(() => {
-    console.log("I run when 'counter' changes");
-  }, [counter]);
-
-  useEffect(() => {
-    console.log('I run when counter & keyword changes');
-  }, [counter, keyword]);
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here..."
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>Click me</button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          type={toDo}
+          placeholder="write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
