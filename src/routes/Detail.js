cf4ecbe22343f55e077d76b7970e77f4eea6be85
blueprint-12 <blueprint12@daum.net>
2022-03-23 22:19:44 +0900

    routes update

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,6 +1,9 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
 function Detail() {
-  return null;
+  const { id } = useParams();
+  console.log(id);
+  return <h1>Detail</h1>;
 }
 export default Detail;
